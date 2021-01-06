d1938bb966195bf28bc143e327c686e843abe483
changchun <734040102@qq.com>
2021-01-06 21:26:06 +0800

    删除无用文件

--- src/main/java/com/example/demo/mapper/primary/BasicMmMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.mapper.primary;
-
-import com.example.demo.entity.primary.BasicMm;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface BasicMmMapper extends Mapper<BasicMm> {
-}
--- src/main/java/com/example/demo/mapper/primary/OrderMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.mapper.primary;
-
-import com.example.demo.entity.primary.Order;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface OrderMapper extends Mapper<Order> {
-}
